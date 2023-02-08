55bf7adaa4513e45088db82dc361b7fc6972e8b3
RenitaT <renitat2025@student.cis.edu.hk>
2023-02-08 11:40:51 +0800

    Complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,75 @@
 public class Main {
+    //1
+    //2
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //3
+        int result = add(3, 8);
+        System.out.println(result);
+
+        //4
+        int newResult = add(3, 8, 4, 9);
+        System.out.println(newResult);
+
+        //5
+        String addisonsAllWeDoIsAdvertise = morningGreeting("Toby Fox");
+        System.out.println(addisonsAllWeDoIsAdvertise);
+
+        //6
+        String macMiller = morningGreeting("Mac Miller");
+        System.out.println(macMiller);
+
+        //7
+        String oohYeahWhat = triple("oohbaby");
+        System.out.println(oohYeahWhat);
+
+        //8
+        int halved = half(8);
+        System.out.println(halved);
+
+        //9
+        int wowWhatALongMethodName = roundPositiveValueToNearestInteger(8.49);
+        System.out.println(wowWhatALongMethodName);
+
+        //10
+        int wowWhatALongerMethodName = roundNegativeValueToNearestInteger(-8.49);
+        System.out.println(wowWhatALongerMethodName);
+
+
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+
+    public static int add(int a, int b, int c, int d) {
+        return add(a, b) + add(c, d);
+    }
+    public static String morningGreeting(String name) {
+        return "早上好，"+name+"!";
+    }
+    public static String afternoonGreeting(String name) {
+        return "下午好，" + name + "!";
+    }
+    public static String triple(String name) {
+        return ""+ name +""+name +""+ name +"";
+    }
+
+    public static int half(int a) {
+        return (a/2);
+    }
+
+    public static int roundPositiveValueToNearestInteger(double a) {
+
+        return (int)a;
+    }
+
+    public static int roundNegativeValueToNearestInteger(double a) {
+        return roundPositiveValueToNearestInteger(a);
+    }
+
+
+
+
+
 }
